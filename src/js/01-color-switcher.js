6f52ac54c90a28fd220b4e57405ed3b1ba247669
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2022-12-04 12:36:04 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.body;
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let changeBgColor = null;
 
+startBtn.addEventListener('click', startBtnHandler);
+stopBtn.addEventListener('click', stopBtnHandler);
+
+function startBtnHandler() {
+  changeBgColor = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+}
+
+function stopBtnHandler() {
+  clearInterval(changeBgColor);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
