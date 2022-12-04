f4a7bca3a612f81ac9ff40d215fad7a02997e381
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2022-12-04 10:36:33 +0000

    Deploying to gh-pages from @ Alex-XAG/goit-js-hw-09@6f52ac54c90a28fd220b4e57405ed3b1ba247669 🚀

---
+++ 01-color-switcher.e6712811.js
@@ -0,0 +1,2 @@
+!function(){var t=document.body,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),r=null;e.addEventListener("click",(function(){r=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.setAttribute("disabled",!0),n.removeAttribute("disabled")})),n.addEventListener("click",(function(){clearInterval(r),e.removeAttribute("disabled"),n.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.e6712811.js.map
